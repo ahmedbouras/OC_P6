def11bc304c9381b80dcd5ea9c8a6cf35fd6157c
Ahmed <ab.perso@outlook.fr>
2020-09-22 17:59:02 +0200

    comment form with right data

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\TrickRepository;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -22,11 +26,32 @@ class BlogController extends AbstractController
     /**
      * @Route("/trick/{title}", name="trick_show")
      */
-    public function trick(TrickRepository $trickRepository, $title)
+    public function trick(Request $request, UserRepository $userRepository, TrickRepository $trickRepository, $title)
     {
         $trick = $trickRepository->findOneBy(['title' => $title]);
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+
+            $comment->setComment($form->get('comment')->getData())
+                    ->setCreatedAt(new \DateTime)
+                    ->setTrick($trick)
+                    ->setUser($this->getUser());
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($comment);
+                $em->flush();
+
+            return $this->redirectToRoute('trick_show', ['title' => $trick->getTitle()]);
+        }
+
         return $this->render('blog/trick.html.twig', [
-            'trick' => $trick
+            'trick' => $trick,
+            'commentForm' => $form->createView(),
         ]);
     }
 }
