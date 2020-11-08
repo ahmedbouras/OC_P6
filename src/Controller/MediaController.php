271351e5f22e956768d3eaa555edb27c0b14a0e9
Ahmed <ab.perso@outlook.fr>
2020-11-08 18:06:08 +0100

    refactoring controllers + gitignore uploaded files

---
+++ src/Controller/MediaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MediaController extends AbstractController
+{
+    /**
+     * @Route("/media", name="media")
+     */
+    public function index()
+    {
+        
+    }
+}
