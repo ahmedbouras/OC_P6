9ea1f6da637cba4abe5bbf75de0419f922515352
Ahmed <ab.perso@outlook.fr>
2020-11-08 22:20:06 +0100

    feat: possibilities to delete trick's video

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Trick;
+use App\Entity\Video;
+use App\Repository\TrickRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MediaController extends AbstractController
 {
     /**
-     * @Route("/media", name="media")
+     * @Route("/video/delete/{id}/trick/{trickId}", name="video_delete")
      */
-    public function index()
+    public function deleteVideo(Video $video, $trickId)
     {
-        
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        try {
+            $em =$this->getDoctrine()->getManager();
+            $em->remove($video);
+            $em->flush();
+
+            $this->addFlash('success', 'Vidéo supprimé.');
+            return $this->redirectToRoute('home');
+        } catch (\Exception $e) {
+            $this->addFlash('danger', 'Une erreur est survenue lors de la suppression de la vidéo.');
+            return $this->redirectToRoute('trick_update', ['id' => $trickId]);
+        }
     }
 }
