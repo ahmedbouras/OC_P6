f31ca5c70a6242907f6205681cb63522b588a6bb
Ahmed <ab.perso@outlook.fr>
2020-11-13 02:30:13 +0100

    feat: medias' form appearance with script javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,52 @@
-$('.hidden-form').hide();
+// Creation des éléments
+var div = $('<div></div>').attr({
+    class: 'card-body p-0',
+});
+var form = $('<form></form>').attr({
+    class: 'd-flex flex-column',
+    method: 'post',
+});
+var input = $('<input>');
+var btn = $('<button></button>').attr(
+    'type', 'submit'
+);
 
-$('.edit-video').click(function() {
-    let id = $(this).attr('data-video');
-    $('.hidden-form').hide();
-    $(`.hidden-form[data-video=${id}]`).show();
+// Placement des éléments dans le bon ordre
+$(div).append(form);
+$(form).append(input);
+$(form).append(btn);
+
+// Evenement pour les images associés à un Trick
+$('.edit-image').click(function() {
+    let idImg = $(this).attr('data-img');
+    let idTrick = $(this).attr('data-trick');
+
+    $(form).attr({
+        action: `/image/update/${idImg}/trick/${idTrick}`,
+        enctype: 'multipart/form-data',
+    });
+    $(input).attr({
+        type: 'file',
+        name: 'newImg',
+    })
+    $(btn).text("Modifier l'image");
+
+    $(`.card[data-img=${idImg}]`).append(div);
 })
 
-$('.edit-image').click(function() {
-    let idimg = $(this).attr('data-img');
-    $('.hidden-form').hide();
-    $(`.hidden-form[data-img=${idimg}]`).show();
+// Evenement pour les vidéos associés à un Trick
+$('.edit-video').click(function() {
+    let idVideo = $(this).attr('data-video');
+    let idTrick = $(this).attr('data-trick');
+    
+    $(form).attr(
+        'action', `/video/update/${idVideo}/trick/${idTrick}`
+    );
+    $(input).attr({
+        type: 'text',
+        name: 'newUrl',
+    })
+    $(btn).text("Modifier la video");
+
+    $(`.card[data-video=${idVideo}]`).append(div);
 })
