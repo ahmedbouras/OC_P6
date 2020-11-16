fb18df4a82f6a4463ab5d46091e59b4ee67103be
Ahmed <ab.perso@outlook.fr>
2020-11-16 03:55:25 +0100

    feat: active load-more button for tricks

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -14,10 +14,28 @@ class BlogController extends AbstractController
     public function home(TrickRepository $trickRepository)
     {
         $totalTricks = count($trickRepository->findAll());
-        $tricks = $trickRepository->findByRangeOf(0, 5);
+        
+        $limit = 5;
+        $tricks = $trickRepository->findByRangeOf(0, $limit);
+        
         return $this->render('blog/home.html.twig', [
             'tricks' => $tricks,
             'totalTricks' => $totalTricks,
+        ]);
+    }
+
+    /**
+     * @Route("/more-trick", name="more_trick")
+     */
+    public function moreTrick(TrickRepository $trickRepository)
+    {
+        $offset = $_POST['offset'];
+        $limit = 5;
+
+        $tricks = $trickRepository->findByRangeOf($offset, $limit);
+        
+        return $this->render('blog/trick.html.twig', [
+            'tricks' => $tricks,
         ]);
     }
 }
